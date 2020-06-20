95d588d9ad9b1a82e4e36b6fb14e703f0f03eecc
Ganesh Jagadeesan <ganesh.jcs@gmail.com>
2020-06-19 21:26:09 -0400

    Fixed implementation

--- nmt/layers/bistable_recurrent_unit.py
+++ nmt/layers/bistable_recurrent_unit.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 from tensorflow import keras 
 from tensorflow.keras import backend as K
 
+import warnings
 
 class NBRCell(keras.layers.GRUCell):
     def __init__(self,
@@ -20,6 +21,7 @@ class NBRCell(keras.layers.GRUCell):
                bias_constraint=None,
                dropout=0.,
                recurrent_dropout=0.,
+               implementation=2,
                **kwargs):
 
         super(NBRCell, self).__init__(
@@ -38,7 +40,7 @@ class NBRCell(keras.layers.GRUCell):
             bias_constraint=bias_constraint,
             dropout=dropout,
             recurrent_dropout=recurrent_dropout,
-            implementation=1,
+            implementation=implementation,
             reset_after=False,
             **kwargs
         )
@@ -66,52 +68,89 @@ class NBRCell(keras.layers.GRUCell):
             else:
                 input_bias, recurrent_bias = tf.unstack(self.bias)
 
-        if 0. < self.dropout < 1.:
-            inputs_z = inputs * dp_mask[0]
-            inputs_r = inputs * dp_mask[1]
-            inputs_h = inputs * dp_mask[2]
-        else:
-            inputs_z = inputs
-            inputs_r = inputs
-            inputs_h = inputs
+        if self.implementation == 1:
+            if 0. < self.dropout < 1.:
+                inputs_z = inputs * dp_mask[0]
+                inputs_r = inputs * dp_mask[1]
+                inputs_h = inputs * dp_mask[2]
+            else:
+                inputs_z = inputs
+                inputs_r = inputs
+                inputs_h = inputs
 
-        x_z = tf.matmul(inputs_z, self.kernel[:, :self.units])
-        x_r = tf.matmul(inputs_r, self.kernel[:, self.units:self.units * 2])
-        x_h = tf.matmul(inputs_h, self.kernel[:, self.units * 2:])
+            x_z = tf.matmul(inputs_z, self.kernel[:, :self.units])
+            x_r = tf.matmul(inputs_r, self.kernel[:, self.units:self.units * 2])
+            x_h = tf.matmul(inputs_h, self.kernel[:, self.units * 2:])
 
-        if self.use_bias:
-            x_z = K.bias_add(x_z, input_bias[:self.units])
-            x_r = K.bias_add(x_r, input_bias[self.units: self.units * 2])
-            x_h = K.bias_add(x_h, input_bias[self.units * 2:])
+            if self.use_bias:
+                x_z = K.bias_add(x_z, input_bias[:self.units])
+                x_r = K.bias_add(x_r, input_bias[self.units: self.units * 2])
+                x_h = K.bias_add(x_h, input_bias[self.units * 2:])
 
-        if 0. < self.recurrent_dropout < 1.:
-            h_tm1_z = h_tm1 * rec_dp_mask[0]
-            h_tm1_r = h_tm1 * rec_dp_mask[1]
-            h_tm1_h = h_tm1 * rec_dp_mask[2]
-        else:
-            h_tm1_z = h_tm1
-            h_tm1_r = h_tm1
-            h_tm1_h = h_tm1
+            if 0. < self.recurrent_dropout < 1.:
+                h_tm1_z = h_tm1 * rec_dp_mask[0]
+                h_tm1_r = h_tm1 * rec_dp_mask[1]
+                h_tm1_h = h_tm1 * rec_dp_mask[2]
+            else:
+                h_tm1_z = h_tm1
+                h_tm1_r = h_tm1
+                h_tm1_h = h_tm1
 
-        recurrent_z = tf.matmul(h_tm1_z, self.recurrent_kernel[:, :self.units])
-        recurrent_r = tf.matmul(h_tm1_r,
-                            self.recurrent_kernel[:, self.units:self.units * 2])
-        if self.reset_after and self.use_bias:
-            recurrent_z = K.bias_add(recurrent_z, recurrent_bias[:self.units])
-            recurrent_r = K.bias_add(recurrent_r,
-                                        recurrent_bias[self.units:self.units * 2])
+            recurrent_z = tf.matmul(h_tm1_z, self.recurrent_kernel[:, :self.units])
+            recurrent_r = tf.matmul(h_tm1_r,
+                                self.recurrent_kernel[:, self.units:self.units * 2])
+            if self.reset_after and self.use_bias:
+                recurrent_z = K.bias_add(recurrent_z, recurrent_bias[:self.units])
+                recurrent_r = K.bias_add(recurrent_r,
+                                            recurrent_bias[self.units:self.units * 2])
 
-        ## r_t = 1 + self.activation(tf.matmul(U_r, x_t) + tf.matmul(W_r, h_tm1))
-        ## z_t = self.recurrent_activation(tf.matmul(U_z, x_t) + tf.matmul(W_z, h_tm1))
+            ## r_t = 1 + tanh(tf.matmul(U_r, x_t) + tf.matmul(W_r, h_tm1))
+            ## z_t = self.recurrent_activation(tf.matmul(U_z, x_t) + tf.matmul(W_z, h_tm1))
 
-        z = self.recurrent_activation(x_z + recurrent_z)
-        r = 1 + self.activation(x_r + recurrent_r)
+            z = self.recurrent_activation(x_z + recurrent_z)
+            r = 1 + tf.nn.tanh(x_r + recurrent_r)
 
-        ## h_t = (z_t * h_tm1) + (1 - z_t)*( self.activation( tf.matmul(U_h, x_t) + r_t * h_tm1 )) 
-        recurrent_h = r *  h_tm1_h
+            ## hh = self.activation( tf.matmul(U_h, x_t) + r_t * h_tm1 )) 
+            recurrent_h = r *  h_tm1_h
 
-        hh = self.activation( x_h + recurrent_h )
+            hh = self.activation( x_h + recurrent_h )
 
+        else: 
+            if 0. < self.dropout < 1.:
+                inputs = inputs * dp_mask[0]
+
+            # inputs projected by all gate matrices at once
+            matrix_x = tf.matmul(inputs, self.kernel)
+            if self.use_bias:
+                # biases: bias_z_i, bias_r_i, bias_h_i
+                matrix_x = K.bias_add(matrix_x, input_bias)
+
+            x_z, x_r, x_h = tf.split(matrix_x, 3, axis=-1)
+
+            if self.reset_after:
+                # hidden state projected by all gate matrices at once
+                matrix_inner = tf.matmul(h_tm1, self.recurrent_kernel)
+                if self.use_bias:
+                    matrix_inner = tf.nn.bias_add(matrix_inner, recurrent_bias)
+            else:
+                # hidden state projected separately for update/reset and new
+                matrix_inner = tf.matmul(h_tm1, self.recurrent_kernel[:, :2 * self.units])
+
+            recurrent_z, recurrent_r, recurrent_h = tf.split(
+                matrix_inner, [self.units, self.units, -1], axis=-1)
+
+            ## r_t = 1 + tanh(tf.matmul(U_r, x_t) + tf.matmul(W_r, h_tm1))
+            ## z_t = self.recurrent_activation(tf.matmul(U_z, x_t) + tf.matmul(W_z, h_tm1))
+
+            r = 1 + tf.nn.tanh(x_r + recurrent_r)
+            z = self.recurrent_activation(x_z + recurrent_z)
+
+            ## hh =  self.activation( tf.matmul(U_h, x_t) + r_t * h_tm1 )) 
+            recurrent_h = r * h_tm1
+
+            hh = self.activation(x_h + recurrent_h)
+
+        ## h_t = z_t * h_tm1 + (1 - z_t)*hh
         h = z * h_tm1 + (1 - z) * hh
         return h, [h]
 
@@ -214,6 +253,7 @@ class NBR(keras.layers.RNN):
                 recurrent_constraint=None,
                 bias_constraint=None,
                 dropout=0.,
+                implementation=2,
                 recurrent_dropout=0.,
                 return_sequences=False,
                 return_state=False,
@@ -221,7 +261,10 @@ class NBR(keras.layers.RNN):
                 stateful=False,
                 unroll=False,
                 **kwargs):
-
+        if implementation == 0:
+            warnings.warn('`implementation=0` has been deprecated, '
+                            'and now defaults to `implementation=1`.'
+                            'Please update your layer call.')
         if 'enable_caching_device' in kwargs:
             cell_kwargs = {'enable_caching_device':
                             kwargs.pop('enable_caching_device')}
@@ -243,6 +286,7 @@ class NBR(keras.layers.RNN):
             bias_constraint=bias_constraint,
             dropout=dropout,
             recurrent_dropout=recurrent_dropout,
+            implementation=implementation,
             dtype=kwargs.get('dtype'),
             trainable=kwargs.get('trainable', True),
             **cell_kwargs)
@@ -319,6 +363,10 @@ class NBR(keras.layers.RNN):
         return self.cell.dropout
 
     @property
+    def implementation(self):
+        return self.cell.implementation
+
+    @property
     def recurrent_dropout(self):
         return self.cell.recurrent_dropout
 
@@ -358,13 +406,13 @@ class NBR(keras.layers.RNN):
                 self.recurrent_dropout,
         }
         if self.cell._enable_caching_device != tf.executing_eagerly():
-            config.update({'enable_caching_device': rnn_cell._enable_caching_device})
+            config.update({'enable_caching_device': self.cell._enable_caching_device})
         base_config = super(NBR, self).get_config()
         del base_config['cell']
         return dict(list(base_config.items()) + list(config.items()))
 
     @classmethod
     def from_config(cls, config):
-        if 'implementation' in config:
+        if 'implementation' in config and config['implementation'] == 0:
             config['implementation'] = 1
         return cls(**config)
